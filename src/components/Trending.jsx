236e1c8e668c1c965b30dfaa6e18aa1095852de0
Azikenp <azikenp26@gmail.com>
2023-09-15 20:34:57 +0100

    adding loader to homepage

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -10,19 +10,35 @@ const Trending = () => {
     <div className="py-[2.62rem] px-20">
       <div className="flex items-center justify-between mb-8">
         <h1 className="text-[34px] font-bold text-black">Trending Movies</h1>
-        <Link className="md:flex gap-1 text-[#e11d48] font-semibold hidden" to="/">
+        <Link
+          className="md:flex gap-1 text-[#e11d48] font-semibold hidden"
+          to="/"
+        >
           <p>see more</p>
           <span className="font-bold">&gt;</span>
         </Link>
       </div>
       <div className="md:grid items-center lg:grid-cols-4 md:grid-cols-3 sm:flex sm:flex-col gap-x-[3.4rem]">
-        {trendingMovies
-          ? trendingMovies.slice(0, 10).map((movie) => {
-              return (
-                <Card className="mb-5" movie={movie} key={movie.id} data-testid="movie-card"/>
-              );
-            })
-          : ""}
+        {trendingMovies && trendingMovies.length > 0 ? (
+          trendingMovies.slice(0, 10).map((movie) => {
+            return (
+              <Card
+                className="mb-5"
+                movie={movie}
+                key={movie.id}
+                data-testid="movie-card"
+              />
+            );
+          })
+        ) : (
+          <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+            <div
+              className="w-8 h-8 border-4 border-cyan rounded-full border-b-gray-300 animate-spin"
+              role="status"
+            ></div>
+            <span className="ml-2">searching . . .</span>
+          </div>
+        )}
       </div>
     </div>
   );
